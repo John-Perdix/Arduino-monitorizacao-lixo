0472fc5d5d239f012369e9c410438af1cb4653e4
pedroramoscosta00 <pedroramoscosta00@gmail.com>
2024-06-06 21:49:53 +0100

    update de icons + website

--- mqtt-react-app/src/mapComponent.js
+++ mqtt-react-app/src/mapComponent.js
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import 'mapbox-gl/dist/mapbox-gl.css';
-import '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css';
 import mapboxgl from 'mapbox-gl';
-import MapboxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions';
 import './App.css';
 import iconEmpty from './assets/iconEmpty.png';
 import iconHalfFull from './assets/iconHalfFull.png';
@@ -14,8 +12,7 @@ import iconFullGas from './assets/iconFullGas.png';
 const MapComponent = ({ markerData }) => {
     const [map, setMap] = useState(null);
     const [marker, setMarker] = useState(null);
-    //const [rendered, setRendered] = useState(false); // Track if component has been rendered
-    const constantCoordinates = [-8.414758649055049, 40.186243201487606]; // Define constant coordinates 40.186243201487606, -8.414758649055049
+    const constantCoordinates = [-8.414758649055049, 40.186243201487606]; // Define constant coordinates
 
     const iconMapping = {
         "Empty": iconEmpty,
@@ -26,8 +23,20 @@ const MapComponent = ({ markerData }) => {
         "FullGas": iconFullGas
     };
 
+    // Function to update favicon
+    const updateFavicon = (iconUrl) => {
+        const link = document.querySelector("link[rel~='icon']");
+        if (!link) {
+            const newLink = document.createElement('link');
+            newLink.rel = 'icon';
+            newLink.href = iconUrl;
+            document.head.appendChild(newLink);
+        } else {
+            link.href = iconUrl;
+        }
+    };
+
     useEffect(() => {
-        // Initialize map and marker only if they are not already set
         mapboxgl.accessToken = 'pk.eyJ1IjoiYW5hZmVycmVpcmEwNiIsImEiOiJjbHcwa3ZocDUwMmxuMmlwaDY0YWJwMmd3In0.Fl-VfouRqMFskxa6yUEu8w';
 
         function setupMap(center) {
@@ -41,16 +50,9 @@ const MapComponent = ({ markerData }) => {
             const nav = new mapboxgl.NavigationControl();
             map.addControl(nav);
 
-            const directions = new MapboxDirections({
-                accessToken: mapboxgl.accessToken
-            });
-
-            map.addControl(directions, 'top-left');
-
             const markerElement = document.createElement('div');
-            //markerElement.style.backgroundImage = `url(${esp32Icon})`;
-            markerElement.style.width = '30px';
-            markerElement.style.height = '30px';
+            markerElement.style.width = '50px';
+            markerElement.style.height = '50px';
             markerElement.style.backgroundSize = '100%';
 
             const newMarker = new mapboxgl.Marker({
@@ -75,13 +77,22 @@ const MapComponent = ({ markerData }) => {
 
             // Update marker icon based on the status
             const markerElement = marker.getElement();
-            const iconKey = markerData.gas === 'Detected' ? `${markerData.status}Gas` : markerData.status;
-            const iconUrl = iconMapping[iconKey];
+            const iconUrl = iconMapping[markerData.status + (markerData.gas === "Detected" ? "Gas" : "")];
             if (iconUrl) {
                 markerElement.style.backgroundImage = `url(${iconUrl})`;
-            } else {
-                console.error(`Icon URL not found for key: ${iconKey}`);
+                // Update favicon
+                updateFavicon(iconUrl);
             }
+
+            // Add popup to marker
+            const popup = new mapboxgl.Popup({ offset: 25 }).setHTML(
+                `<div style="color: black;">
+                    <strong>Name:</strong> Trash Can<br/>
+                    <strong>Fullness:</strong> ${markerData.status}<br/>
+                    <strong>Air Quality:</strong> ${markerData.gas === "Detected" ? "Poor" : "Good"}
+                </div>`
+            );
+            marker.setPopup(popup);
         }
     }, [markerData, marker, map]);
 
